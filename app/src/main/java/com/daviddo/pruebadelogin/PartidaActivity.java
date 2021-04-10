3e2cca1b25f3d21fa1af8623bf3213acf3949bd8
ddominguezbl <ddominguezbl@uoc.edu>
2021-04-10 20:38:50 +0200

    FINALIZACION

--- app/src/main/java/com/daviddo/pruebadelogin/PartidaActivity.java
+++ app/src/main/java/com/daviddo/pruebadelogin/PartidaActivity.java
@@ -5,11 +5,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View;
 import android.widget.ImageView;
@@ -25,11 +27,11 @@ public class PartidaActivity extends AppCompatActivity {
     ImageView btConfig;
     ImageView btHelp;
     ImageView imagenPuzzle;
-    ArrayList<Trozo> listaTrozos;
+    ArrayList<ImageView> listaTrozos;
     Bitmap imagenoriginal, imagenvacia;
     ConstraintLayout CLimagenPuzzle;
-    int numfilas = 4;
-    int numcolumnas = 4;
+    int numfilas = 2;
+    int numcolumnas = 2;
     GridLayout gridImagen;
     int anchocadapieza;
     int altocadapieza;
@@ -40,11 +42,13 @@ public class PartidaActivity extends AppCompatActivity {
     int filadelaimagenpulsada;
     int columnadelaimagenpulsada;
     ImageView imagenpulsada;
-    Button button;
+    Button botonGameOver, btEmpezar;
+    int ronda  =1;
+    boolean haganado = true;
 
 
 
-    ImageView[][] arrayImagenes = new ImageView[numfilas][numcolumnas];
+    ImageView[][] arrayImagenes;
 
 
     @Override
@@ -54,9 +58,10 @@ public class PartidaActivity extends AppCompatActivity {
 
         btConfig = findViewById(R.id.btConfig);
         gridImagen = findViewById(R.id.idGridImagen);
-        button = (Button) findViewById(R.id.button);
+        botonGameOver = (Button) findViewById(R.id.button);
+        btEmpezar = (Button) findViewById(R.id.btEmpezar);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        botonGameOver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 abrirbutton();
@@ -65,19 +70,155 @@ public class PartidaActivity extends AppCompatActivity {
 
     }
 
-    public void abrirbutton() {
-        Intent intent = new Intent(this, GameOver.class);
-        startActivity(intent);
 
+    private void iniciarTodo() {
+        haganado = false;
+        TextView numeroronda = findViewById(R.id.numeroRonda);
+        numeroronda.setText(ronda+"");
+        arrayImagenes = new ImageView[numfilas][numcolumnas];
+        gridImagen.setColumnCount(numcolumnas);
+        gridImagen.setRowCount(numfilas);
+        gridImagen.setOrientation(GridLayout.HORIZONTAL);
+        ViewGroup.LayoutParams par = gridImagen.getLayoutParams();
+
+        /// anchogrid = gridImagen.getWidth();
+        //  altogrid = gridImagen.getHeight();
+
+        // elegir la imagen
+        imagenoriginal = BitmapFactory.decodeResource(getResources(), R.drawable.photo);
+        // imagenoriginal = Bitmap.createScaledBitmap(imagenoriginal,anchogrid, altogrid, false);
+        imagenvacia = BitmapFactory.decodeResource(getResources(), R.drawable.image_2935360_960_720);
+
+
+        // dividir la imagebn
+        trocearImagen(imagenoriginal);
+
+        desordenar();
+
+        pintarImagen();
+
+    }
+
+    private void trocearImagen(Bitmap imageninicial) {
+        listaTrozos = new ArrayList<>();
+        anchocadapieza = imageninicial.getWidth() / numcolumnas;
+        altocadapieza = imageninicial.getHeight() / numfilas;
+
+        int yCoord = 0;
+        for (int f = 0; f < numfilas; f++) {
+            int xCoord = 0;
+            for (int c = 0; c < numcolumnas; c++) {
+
+                Bitmap b = Bitmap.createBitmap(imageninicial, xCoord, yCoord, anchocadapieza, altocadapieza);
+                ImageView imagen = new ImageView(this);
+                imagen.setId(View.generateViewId());
+                LinearLayout.LayoutParams parametros = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                parametros.gravity = Gravity.CENTER;
+                imagen.setLayoutParams(parametros);
+                imagen.setImageBitmap(b);
+
+                Posiciones pos = new Posiciones();
+                pos.xInicial = f;
+                pos.yInicial = c;
+                imagen.setTag(pos);
+
+                imagen.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        imagenpulsada(v);
+                    }
+                });
+
+                listaTrozos.add(imagen);
+
+                xCoord = xCoord + anchocadapieza;
+            }
+            yCoord += altocadapieza;
+        }
+
+        Bitmap bitmapvacio = Bitmap.createBitmap(imagenvacia, 0, 0, anchocadapieza, altocadapieza);
+        ImageView imagen = new ImageView(this);
+        imagen.setId(View.generateViewId());
+        LinearLayout.LayoutParams parametros = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        parametros.gravity = Gravity.CENTER;
+        imagen.setLayoutParams(parametros);
+        imagen.setImageBitmap(bitmapvacio);
+
+        Posiciones pos = new Posiciones();
+        pos.xInicial = numfilas-1;
+        pos.yInicial = numcolumnas-1;
+        pos.eshueco = true;
+        imagen.setTag(pos);
+
+        imagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagenpulsada(v);
+            }
+        });
+
+        listaTrozos.set(numcolumnas * numfilas - 1, imagen);
+
+    }
+
+    private void desordenar() {
+
+        // desordenar el arraylist
+        Collections.shuffle(listaTrozos);
+
+        // y ahora ya desordenado le ponemos las coordenadas reales que tenga ahora
+//        int cont = 0;
+//        for (int f = 0; f < numfilas; f++) {
+//            for (int c = 0; c < numcolumnas; c++) {
+//                ImageView imagen = listaTrozos.get(cont);
+//                Posiciones posicionquetenia  = (Posiciones) imagen.getTag();
+//                posicionquetenia.xActual = f;
+//                posicionquetenia.yActual  = c;
+//            }
+//        }
+
+    }
+
+    private void pintarImagen() {
+        // crear el array con el arraylist
+
+        int cont = 0;
+        for (int f = 0; f < numfilas; f++) {
+
+            for (int c = 0; c < numcolumnas; c++) {
+
+                ImageView imagen = listaTrozos.get(cont);
+                Posiciones posicionquetenia  = (Posiciones) imagen.getTag();
+                posicionquetenia.xActual = f;
+                posicionquetenia.yActual  = c;
+
+                if (posicionquetenia.eshueco){
+                    filaHueco = f;
+                    columnaHueco = c;
+
+
+                }
+
+                arrayImagenes[f][c] = imagen;
+                gridImagen.addView(imagen);
+
+
+
+                cont++;
+            }
+        }
 
     }
 
     public void imagenpulsada(View view) {
+
+
+
+
         imagenpulsada = (ImageView) view;
-        String tag = imagenpulsada.getTag().toString();
-        String[] trozos = tag.split(":");
-        filadelaimagenpulsada = Integer.parseInt(trozos[0]);
-        columnadelaimagenpulsada = Integer.parseInt(trozos[1]);
+        Posiciones posicionquetenia  = (Posiciones) imagenpulsada.getTag();
+        filadelaimagenpulsada = posicionquetenia.xActual;
+        columnadelaimagenpulsada = posicionquetenia.yActual;
 
         //     imagenpulsada.setVisibility(View.INVISIBLE);
 
@@ -100,144 +241,123 @@ public class PartidaActivity extends AppCompatActivity {
     }
 
     public void moverImagen() {
+        logDePosiciones();
+
+        Posiciones posicionesDelHueco     = (Posiciones) arrayImagenes[filaHueco][columnaHueco].getTag();
+        Posiciones posicionesDeLaPulsada  = (Posiciones) arrayImagenes[filadelaimagenpulsada][columnadelaimagenpulsada].getTag();
+
         Drawable drawablepulsada = arrayImagenes[filadelaimagenpulsada][columnadelaimagenpulsada].getDrawable();
         Drawable drawablehueco = arrayImagenes[filaHueco][columnaHueco].getDrawable();
+
         arrayImagenes[filaHueco][columnaHueco].setImageDrawable(drawablepulsada);
         arrayImagenes[filadelaimagenpulsada][columnadelaimagenpulsada].setImageDrawable(drawablehueco);
 
-        arrayImagenes[filaHueco][columnaHueco].setTag(filaHueco + ":" + columnaHueco);
-        arrayImagenes[filadelaimagenpulsada][columnadelaimagenpulsada].setTag(filadelaimagenpulsada + ":" + columnadelaimagenpulsada);
+        // op 1
+        posicionesDelHueco.xActual = filadelaimagenpulsada;
+        posicionesDelHueco.yActual = columnadelaimagenpulsada;
+        posicionesDeLaPulsada.xActual = filaHueco;
+        posicionesDeLaPulsada.yActual = columnaHueco;
+        arrayImagenes[filaHueco][columnaHueco].setTag(posicionesDeLaPulsada);
+        arrayImagenes[filadelaimagenpulsada][columnadelaimagenpulsada].setTag(posicionesDelHueco);
+
+        // op2
+//        arrayImagenes[filaHueco][columnaHueco].setTag(posicionesDelHueco);
+//        arrayImagenes[filadelaimagenpulsada][columnadelaimagenpulsada].setTag(posicionesDeLaPulsada);
+
+        //  op3
+//        Posiciones posicionesDelHueco2     = (Posiciones) arrayImagenes[filaHueco][columnaHueco].getTag();
+//        Posiciones posicionesDeLaPulsada2  = (Posiciones) arrayImagenes[filadelaimagenpulsada][columnadelaimagenpulsada].getTag();
+//        posicionesDelHueco2.xInicial = posicionesDeLaPulsada.xInicial;
+//        posicionesDelHueco2.yInicial = posicionesDeLaPulsada.yInicial;
+//        posicionesDeLaPulsada2.xInicial = posicionesDelHueco.xInicial;
+//        posicionesDeLaPulsada2.yInicial = posicionesDelHueco.yInicial;
+
+//        arrayImagenes[filaHueco][columnaHueco].setTag(posicionesDelHueco2);
+//        arrayImagenes[filadelaimagenpulsada][columnadelaimagenpulsada].setTag(posicionesDeLaPulsada2);
+
+
         filaHueco = filadelaimagenpulsada;
         columnaHueco = columnadelaimagenpulsada;
+
+        if (estaAcabado()){
+            Toast.makeText(this, " ENHORABUENA!!!!  HAS GANADO !!!" , Toast.LENGTH_LONG).show();
+            nuevoJuego();
+        }
+
+        logDePosiciones();
+    }
+
+    //==============================================================================================
+
+    public void abrirbutton() {
+        Intent intent = new Intent(this, GameOver.class);
+        startActivity(intent);
+
+    }
+
+    public void nuevoJuego() {
+//        haganado = true;
+//        for (int f = 0; f < numfilas; f++) {
+//
+//            for (int c = 0; c < numcolumnas; c++) {
+//
+//                ImageView imagen =   arrayImagenes[f][c];
+//                imagen.setOnClickListener(null);
+//                imagen = null;
+//
+//
+//            }
+//        }
+//        listaTrozos.clear();
+//
+//        ronda ++;
+//
+//         numfilas ++;
+//         numcolumnas ++;
+
+
+    }
+
+    public boolean estaAcabado(){
+        int cont = 0;
+        boolean exito = true;
+        for (int f = 0; f < numfilas; f++) {
+            for (int c = 0; c < numcolumnas; c++) {
+                ImageView imagen = arrayImagenes[f][c];
+                Posiciones pos  = (Posiciones) imagen.getTag();
+                if(pos.yActual != pos.yInicial || pos.xActual != pos.xInicial){
+                    exito = false;
+                }
+            }
+        }
+        return exito;
+    }
+
+
+    public void logDePosiciones(){
+
+
+        String s = "";
+       for (int f = 0; f < numfilas; f++) {
+            for (int c = 0; c < numcolumnas; c++) {
+                ImageView imagen = arrayImagenes[f][c];
+                Posiciones pos  = (Posiciones) imagen.getTag();
+                s+=" \n=================================> FILA  " + f + " COLUMNA" + c;
+
+                s+=" \n=============> X INICIAL " + pos.xInicial;
+                s+=" \n=============> Y INICIAL " + pos.yInicial;
+                s+=" \n=============> X ACTUAL  " + pos.xActual;
+                s+=" \n=============> Y ACTUAL  " + pos.yActual;
+                s+=" \n\n";
+                Log.e("________________ DAVID", s);
+            }
+        }
+
     }
 
     public void onclickEmpezar(View view) {
-        iniciarTodo();
-    }
-
-    private void iniciarTodo() {
-        gridImagen.setColumnCount(numcolumnas);
-        gridImagen.setRowCount(numfilas);
-        gridImagen.setOrientation(GridLayout.HORIZONTAL);
-        ViewGroup.LayoutParams par = gridImagen.getLayoutParams();
-
-        /// anchogrid = gridImagen.getWidth();
-        //  altogrid = gridImagen.getHeight();
-
-        // elegir la imagen
-        imagenoriginal = BitmapFactory.decodeResource(getResources(), R.drawable.photo);
-        // imagenoriginal = Bitmap.createScaledBitmap(imagenoriginal,anchogrid, altogrid, false);
-        imagenvacia = BitmapFactory.decodeResource(getResources(), R.drawable.image_2935360_960_720);
-
-
-        // dividir la imagebn
-        trocearImagen(imagenoriginal);
-
-        // desordenar images
-        desordenar();
-        //   Collections.shuffle(listaTrozos);
-
-        // pintr la imagen
-        pintarImagen();
-
-
-        // colocar escuchadores a cada elemento imagen
-
-        // jugar:
-        //      voltear ficha
-        //      comprobar fin
-        //      guardar resutaldos
-
-
-    }
-
-
-    private ArrayList<Trozo> trocearImagen(Bitmap imageninicial) {
-        listaTrozos = new ArrayList<>();
-        anchocadapieza = imageninicial.getWidth() / numcolumnas;
-        altocadapieza = imageninicial.getHeight() / numfilas;
-
-        int yCoord = 0;
-        for (int f = 0; f < numfilas; f++) {
-            int xCoord = 0;
-            for (int c = 0; c < numcolumnas; c++) {
-
-                Bitmap b = Bitmap.createBitmap(imageninicial, xCoord, yCoord, anchocadapieza, altocadapieza);
-                ImageView imagen = new ImageView(this);
-                imagen.setId(View.generateViewId());
-                LinearLayout.LayoutParams parametros = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                parametros.gravity = Gravity.CENTER;
-                imagen.setLayoutParams(parametros);
-                imagen.setImageBitmap(b);
-                //  imagen.setTag(f + ":" + c);
-                imagen.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        imagenpulsada(v);
-                    }
-                });
-                Trozo trozo = new Trozo(imagen, f, c, false);
-                listaTrozos.add(trozo);
-
-                xCoord = xCoord + anchocadapieza;
-            }
-            yCoord += altocadapieza;
-        }
-
-        Bitmap bitmapvacio = Bitmap.createBitmap(imagenvacia, 0, 0, anchocadapieza, altocadapieza);
-        ImageView imagen = new ImageView(this);
-        imagen.setId(View.generateViewId());
-        LinearLayout.LayoutParams parametros = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        parametros.gravity = Gravity.CENTER;
-        imagen.setLayoutParams(parametros);
-        imagen.setImageBitmap(bitmapvacio);
-        imagen.setTag("HUECO");
-        imagen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                imagenpulsada(v);
-            }
-        });
-        Trozo trozo = new Trozo(imagen, numfilas - 1, numcolumnas - 1, true);
-        listaTrozos.set(numcolumnas * numfilas - 1, trozo);
-        return listaTrozos;
-    }
-
-
-    private void desordenar() {
-
-        // desordenar el arraylist
-        Collections.shuffle(listaTrozos);
-
-
-    }
-
-
-    private void pintarImagen() {
-        // crear el array con el arraylist
-
-        int cont = 0;
-        for (int f = 0; f < numfilas; f++) {
-
-            for (int c = 0; c < numcolumnas; c++) {
-                //  if( ! ( f==numfilas-1 &&  c==numcolumnas-1) ) {
-                ImageView imagen = listaTrozos.get(cont).imagen;
-                if (imagen.getTag() != null) {
-
-
-                    if (imagen.getTag().equals("HUECO")) {
-                        filaHueco = f;
-                        columnaHueco = c;
-
-                    }
-                }
-                imagen.setTag(f + ":" + c);
-                arrayImagenes[f][c] = imagen;
-                gridImagen.addView(imagen);
-                cont++;
-                //   }
-
-            }
+        if(haganado== true){
+            iniciarTodo();
         }
 
     }
---
+++ app/src/main/java/com/daviddo/pruebadelogin/PartidaActivityOLD.java
@@ -0,0 +1,251 @@
+package com.daviddo.pruebadelogin;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class PartidaActivityOLD extends AppCompatActivity {
+
+    ImageView btConfig;
+    ImageView btHelp;
+    ImageView imagenPuzzle;
+    ArrayList<ImageView> listaTrozos;
+    Bitmap imagenoriginal, imagenvacia;
+    ConstraintLayout CLimagenPuzzle;
+    int numfilas = 4;
+    int numcolumnas = 4;
+    GridLayout gridImagen;
+    int anchocadapieza;
+    int altocadapieza;
+    int anchogrid = 324;
+    int altogrid = 500;
+    int filaHueco = 0;
+    int columnaHueco = 01;
+    int filadelaimagenpulsada;
+    int columnadelaimagenpulsada;
+    ImageView imagenpulsada;
+    Button button;
+
+
+
+    ImageView[][] arrayImagenes = new ImageView[numfilas][numcolumnas];
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partida);
+
+        btConfig = findViewById(R.id.btConfig);
+        gridImagen = findViewById(R.id.idGridImagen);
+        button = (Button) findViewById(R.id.button);
+
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                abrirbutton();
+//            }
+//        });
+
+    }
+//
+//    public void abrirbutton() {
+//        Intent intent = new Intent(this, GameOver.class);
+//        startActivity(intent);
+//
+//
+//    }
+//
+//    public void imagenpulsada(View view) {
+//        imagenpulsada = (ImageView) view;
+//        String tag = imagenpulsada.getTag().toString();
+//        String[] trozos = tag.split(":");
+//        filadelaimagenpulsada = Integer.parseInt(trozos[0]);
+//        columnadelaimagenpulsada = Integer.parseInt(trozos[1]);
+//
+//        //     imagenpulsada.setVisibility(View.INVISIBLE);
+//
+//        // mirrar si ARRIBA hay hueco
+//        if (filadelaimagenpulsada - 1 == filaHueco && columnadelaimagenpulsada == columnaHueco) {
+//            moverImagen();
+//        }
+//        // mirrar si ABAJO hay hueco
+//        if (filadelaimagenpulsada + 1 == filaHueco && columnadelaimagenpulsada == columnaHueco) {
+//            moverImagen();
+//        }
+//        // mirrar si DERECHA hay hueco
+//        if (filadelaimagenpulsada == filaHueco && columnadelaimagenpulsada + 1 == columnaHueco) {
+//            moverImagen();
+//        }
+//        // mirrar si IZQUIERDA hay hueco
+//        if (filadelaimagenpulsada == filaHueco && columnadelaimagenpulsada - 1 == columnaHueco) {
+//            moverImagen();
+//        }
+//    }
+//
+//    public void moverImagen() {
+//        Drawable drawablepulsada = arrayImagenes[filadelaimagenpulsada][columnadelaimagenpulsada].getDrawable();
+//        Drawable drawablehueco = arrayImagenes[filaHueco][columnaHueco].getDrawable();
+//        arrayImagenes[filaHueco][columnaHueco].setImageDrawable(drawablepulsada);
+//        arrayImagenes[filadelaimagenpulsada][columnadelaimagenpulsada].setImageDrawable(drawablehueco);
+//
+//        arrayImagenes[filaHueco][columnaHueco].setTag(filaHueco + ":" + columnaHueco);
+//        arrayImagenes[filadelaimagenpulsada][columnadelaimagenpulsada].setTag(filadelaimagenpulsada + ":" + columnadelaimagenpulsada);
+//        filaHueco = filadelaimagenpulsada;
+//        columnaHueco = columnadelaimagenpulsada;
+//    }
+//
+//    public void onclickEmpezar(View view) {
+//        iniciarTodo();
+//    }
+//
+//    private void iniciarTodo() {
+//        gridImagen.setColumnCount(numcolumnas);
+//        gridImagen.setRowCount(numfilas);
+//        gridImagen.setOrientation(GridLayout.HORIZONTAL);
+//        ViewGroup.LayoutParams par = gridImagen.getLayoutParams();
+//
+//        /// anchogrid = gridImagen.getWidth();
+//        //  altogrid = gridImagen.getHeight();
+//
+//        // elegir la imagen
+//        imagenoriginal = BitmapFactory.decodeResource(getResources(), R.drawable.photo);
+//        // imagenoriginal = Bitmap.createScaledBitmap(imagenoriginal,anchogrid, altogrid, false);
+//        imagenvacia = BitmapFactory.decodeResource(getResources(), R.drawable.image_2935360_960_720);
+//
+//
+//        // dividir la imagebn
+//        trocearImagen(imagenoriginal);
+//
+//        // desordenar images
+//        desordenar();
+//        //   Collections.shuffle(listaTrozos);
+//
+//        // pintr la imagen
+//        pintarImagen();
+//
+//
+//        // colocar escuchadores a cada elemento imagen
+//
+//        // jugar:
+//        //      voltear ficha
+//        //      comprobar fin
+//        //      guardar resutaldos
+//
+//
+//    }
+//
+//
+//    private ArrayList<Trozo> trocearImagen(Bitmap imageninicial) {
+//        listaTrozos = new ArrayList<>();
+//        anchocadapieza = imageninicial.getWidth() / numcolumnas;
+//        altocadapieza = imageninicial.getHeight() / numfilas;
+//
+//        int yCoord = 0;
+//        for (int f = 0; f < numfilas; f++) {
+//            int xCoord = 0;
+//            for (int c = 0; c < numcolumnas; c++) {
+//
+//                Bitmap b = Bitmap.createBitmap(imageninicial, xCoord, yCoord, anchocadapieza, altocadapieza);
+//                ImageView imagen = new ImageView(this);
+//                imagen.setId(View.generateViewId());
+//                LinearLayout.LayoutParams parametros = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+//                parametros.gravity = Gravity.CENTER;
+//                imagen.setLayoutParams(parametros);
+//                imagen.setImageBitmap(b);
+//                //  imagen.setTag(f + ":" + c);
+//                imagen.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        imagenpulsada(v);
+//                    }
+//                });
+//                Trozo trozo = new Trozo(imagen, f, c, false);
+//                listaTrozos.add(trozo);
+//
+//                xCoord = xCoord + anchocadapieza;
+//            }
+//            yCoord += altocadapieza;
+//        }
+//
+//        Bitmap bitmapvacio = Bitmap.createBitmap(imagenvacia, 0, 0, anchocadapieza, altocadapieza);
+//        ImageView imagen = new ImageView(this);
+//        imagen.setId(View.generateViewId());
+//        LinearLayout.LayoutParams parametros = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+//        parametros.gravity = Gravity.CENTER;
+//        imagen.setLayoutParams(parametros);
+//        imagen.setImageBitmap(bitmapvacio);
+//        imagen.setTag("HUECO");
+//        imagen.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                imagenpulsada(v);
+//            }
+//        });
+//        Trozo trozo = new Trozo(imagen, numfilas - 1, numcolumnas - 1, true);
+//        listaTrozos.set(numcolumnas * numfilas - 1, trozo);
+//        return listaTrozos;
+//    }
+//
+//
+//    private void desordenar() {
+//
+//        // desordenar el arraylist
+//        Collections.shuffle(listaTrozos);
+//
+//
+//    }
+//
+//
+//    private void pintarImagen() {
+//        // crear el array con el arraylist
+//
+//        int cont = 0;
+//        for (int f = 0; f < numfilas; f++) {
+//
+//            for (int c = 0; c < numcolumnas; c++) {
+//                //  if( ! ( f==numfilas-1 &&  c==numcolumnas-1) ) {
+//                ImageView imagen = listaTrozos.get(cont).imagen;
+//                if (imagen.getTag() != null) {
+//
+//
+//                    if (imagen.getTag().equals("HUECO")) {
+//                        filaHueco = f;
+//                        columnaHueco = c;
+//
+//                    }
+//                }
+//                imagen.setTag(f + ":" + c);
+//                arrayImagenes[f][c] = imagen;
+//                gridImagen.addView(imagen);
+//                cont++;
+//                //   }
+//
+//            }
+//        }
+//
+//    }
+//
+//
+//    public void config(View view) {
+//        Toast.makeText(getApplicationContext(), "Pulsado configurar", Toast.LENGTH_SHORT).show();
+//    }
+
+
+}
